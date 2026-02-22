327a221a944c7312a3a5018f931266c0923a5e60
Saveri23 <gavvalasaveri89@gmail.com>
2026-02-22 21:10:09 +0530

    Initial commit4

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // ✅ Important for deployment: makes paths relative
+  base: "./", // ✅ Important for deployment
   server: {
     proxy: {
       "/api": {
